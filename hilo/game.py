a674340a7567c56491461172e1c4c6ed9a54fd86
Elizeu Silva <elizeu.aliam@gmail.com>
2022-01-24 11:42:18 -0400

    New branch

--- hilo/game.py
+++ hilo/game.py
@@ -1,12 +1,34 @@
+# import random
+   
+# class Dealer:
+#     def __init__(self, card=None):
+#         self.card = card
+#     def show_card(self):
+#         self.card = random.randint(1,13)
+#         return self.card
+#testando
 import random
    
 class Dealer:
+    """A person who directs the game. 
+    
+    The responsibility of a Dealer is to control the sequence of play.
+    Attributes:
+        card: A value of randomly chosen card (from 1 to 13).
+    """
     def __init__(self, card=None):
-        self.card = card
-    def show_card(self):
-        self.card = random.randint(1,13)
-        return self.card
+        """Constructs a new Dealer.
         
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+        self.card = card
 
-           
+    def show_card(self):
+        """Randomly chooses and returns value of card.
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
+        self.card = random.randint(1,13)
+        return self.card       
     
--- hilo/player.py
+++ hilo/player.py
@@ -1,7 +1,28 @@
 
-class Play:
+# class Play:
     
-    def __init__(self, score = 300, cont_game = True, guess = None):
+#     def __init__(self, score = 300, cont_game = True, guess = None):
+#         self.score = score
+#         self.guess = guess
+#         self.cont_game = cont_game
+
+class Play:
+    """Features of person who plays the game.
+    The responsibility of Play is to keep track of score and to decide whether the 
+    game should be continued.
+   
+    Attributes:
+        score (int): current score of a player
+        guess (str): higher or lower (h/l). Player's guess if the next card will worth
+                     more or less than the current one. 
+        cont_game: player's answer to a prompt to play
+    """
+    def __init__(self, score = 300, cont_game = "", guess = None):
+        """Constructs a new Play.
+        
+        Args:
+            self (Play): an instance of Play.
+        """
         self.score = score
         self.guess = guess
         self.cont_game = cont_game
